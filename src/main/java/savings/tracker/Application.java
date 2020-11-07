a02fac59ee7a2af9aedd5074ac2c0ad4dad0c698
yumeng-luo <claire.luo414@gmail.com>
2020-11-06 16:08:19 -0800

    Added log out button and reuse cookie

--- src/main/java/savings/tracker/Application.java
+++ src/main/java/savings/tracker/Application.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.web.authentication.HttpStatusEntryPoint;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 
 @SpringBootApplication
 public class Application extends WebSecurityConfigurerAdapter {
@@ -41,8 +42,16 @@ public class Application extends WebSecurityConfigurerAdapter {
         .anyRequest().authenticated())
         .exceptionHandling(
             e -> e.authenticationEntryPoint(new HttpStatusEntryPoint(HttpStatus.UNAUTHORIZED)))
+        .csrf(c -> c
+            .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+        )
+        .logout(l -> l
+            .logoutSuccessUrl("/").permitAll()
+        )
         .oauth2Login();
     // @formatter:on
   }
+  
+  
 
 }
