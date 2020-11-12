e1bb024a3eb130169063e345fb59b2c8aad6355a
Kevin Hwang <kjhwang25@gmail.com>
2020-11-11 23:55:27 -0500

    Added dynamic content functionality and updated some images

--- src/main/java/savings/tracker/SendGridEmailer.java
+++ src/main/java/savings/tracker/SendGridEmailer.java
@@ -6,31 +6,49 @@ import com.sendgrid.helpers.mail.objects.*;
 import java.io.IOException;
 
 public class SendGridEmailer {
+  
+  private static void send(final Mail mail) throws IOException {
+    final SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
+    sg.addRequestHeader("X-Mock", "true");
+
+    final Request request = new Request();
+    request.setMethod(Method.POST);
+    request.setEndpoint("mail/send");
+    request.setBody(mail.build());
+
+    final Response response = sg.api(request);
+    System.out.println(response.getStatusCode());
+    System.out.println(response.getBody());
+    System.out.println(response.getHeaders());
+  }
+  
+  public static Mail buildDynamicTemplate() {
+    Mail mail = new Mail();
+
+    Email fromEmail = new Email();
+    fromEmail.setName("Savings Tracker Team");
+    fromEmail.setEmail("ASE.email.api@gmail.com");
+    mail.setFrom(fromEmail);
+
+    mail.setTemplateId("d-8bbecf6e66154f6596466202edb71ccf");
+
+    Personalization personalization = new Personalization();
+    personalization.addTo(new Email("<demo>@columbia.edu"));
+    personalization.addDynamicTemplateData("weekly-total","This week you saved $5.00."); // substitute with string build
+    personalization.addDynamicTemplateData("cumul-total","In total, you saved $15.00!"); // substitute with string build
+    mail.addPersonalization(personalization);
+
+    return mail;
+  }
+  
+  public static void sendDynamicEmail() throws IOException {
+    final Mail dynamicTemplate = buildDynamicTemplate();
+    send(dynamicTemplate);
+  }
+  
   public static void main(String[] args) throws IOException {
-    try {
-      SendGrid sg = new SendGrid(System.getenv("SENDGRID_API_KEY"));
-      Email from = new Email("ASE.email.api@gmail.com");
-      Email to = new Email("<demo>@gmail.com"); // use your own email address here
-  
-      String subject = "Testing sendgrid";
-      Content content = new Content("text/html", "<em>From</em> <strong>Joe Biden</strong>");
-  
-      Mail mail = new Mail(from, subject, to, content);
-  
-      
-      Request request = new Request();
-  
-      request.setMethod(Method.POST);
-      request.setEndpoint("mail/send");
-      request.setBody(mail.build());
-  
-      Response response = sg.api(request);
-  
-      System.out.println(response.getStatusCode());
-      System.out.println(response.getHeaders());
-      System.out.println(response.getBody());
-    } catch (IOException e) {
-      throw e;
-    }
+    
+    sendDynamicEmail();
+
   }
 }
