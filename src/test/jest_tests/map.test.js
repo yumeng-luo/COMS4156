d6629bc362939c7a6eacbacad762a5f06e212cee
Sang Jun Chun <junchun96@gmail.com>
2020-11-19 15:04:35 -0700

    Add mock to jest test

--- src/test/jest_tests/map.test.js
+++ src/test/jest_tests/map.test.js
@@ -1,5 +1,41 @@
-const sum = require('../../main/resources/static/js/map');
+const GoogleMaps = require('../../main/resources/static/js/map');
+
+const setupGoogleMock = () => {
+  /*** Mock Google Maps JavaScript API ***/
+  const google = {
+    maps: {
+      places: {
+        AutocompleteService: () => {},
+        PlacesServiceStatus: {
+          INVALID_REQUEST: 'INVALID_REQUEST',
+          NOT_FOUND: 'NOT_FOUND',
+          OK: 'OK',
+          OVER_QUERY_LIMIT: 'OVER_QUERY_LIMIT',
+          REQUEST_DENIED: 'REQUEST_DENIED',
+          UNKNOWN_ERROR: 'UNKNOWN_ERROR',
+          ZERO_RESULTS: 'ZERO_RESULTS',
+        },
+      },
+      Geocoder: () => {},
+      GeocoderStatus: {
+        ERROR: 'ERROR',
+        INVALID_REQUEST: 'INVALID_REQUEST',
+        OK: 'OK',
+        OVER_QUERY_LIMIT: 'OVER_QUERY_LIMIT',
+        REQUEST_DENIED: 'REQUEST_DENIED',
+        UNKNOWN_ERROR: 'UNKNOWN_ERROR',
+        ZERO_RESULTS: 'ZERO_RESULTS',
+      },
+    },
+  };
+  global.window.google = google;
+};
+
+// in test file.
+beforeAll(() => {
+  setupGoogleMock();
+});
 
 test('dummy test', () => {
-  expect(createMarkers()).toBeTruthy();
+  expect(google.maps.places.PlacesServiceStatus.OK).toBeTruthy();
 });
