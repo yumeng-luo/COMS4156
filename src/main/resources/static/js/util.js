432057dd96071bbfb2d116b02a11e607bccd5a1b
Sang Jun Chun <junchun96@gmail.com>
2020-11-20 22:09:55 -0700

    Finish endpoint implementation for index.html

--- src/main/resources/static/js/util.js
+++ src/main/resources/static/js/util.js
@@ -14,7 +14,7 @@ function init_search() {
       var xhttp = new XMLHttpRequest();	
       xhttp.onreadystatechange = function() {	
         if (this.readyState == 4 && this.status == 200) {	
-        document.getElementById("searched").innerHTML = this.responseText;	
+          document.getElementById("searched").innerHTML = this.responseText;	
         }
       };
       // alert("item=" + searched_item + "&lat=37.7510&lon=-97.8220");
@@ -22,4 +22,20 @@ function init_search() {
       xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
       xhttp.send('item='+searched_item+'&lat=37.7510&lon=-97.8220');
     // }	
+  } 
+
+function select_search(item_index) {
+      searched_item=document.getElementById("search_bar").value
+
+      var xhttp = new XMLHttpRequest(); 
+      xhttp.onreadystatechange = function() { 
+        if (this.readyState == 4 && this.status == 200) { 
+          window.location.href = "alt.html";
+        }
+      };
+      // alert("item=" + searched_item + "&lat=37.7510&lon=-97.8220");
+      xhttp.open("POST", "/select_item", true);
+      xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+      xhttp.send('item_number='+item_index);
+    // }  
   } 
